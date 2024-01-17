bf2405eb29b5c4ba30db84e15fa60845c0575024
Jane-Terziev <janeterziev@gmail.com>
2024-01-17 01:19:37 +0100

    Added object to struct mapper, added dependencies

--- lib/dry_object_mapper.rb
+++ lib/dry_object_mapper.rb
@@ -1,6 +1,8 @@
 # frozen_string_literal: true
 
 require_relative "dry_object_mapper/version"
+require "dry_struct_parser/struct_schema_parser"
+require_relative "dry_object_mapper/mapper"
 
 module DryObjectMapper
   class Error < StandardError; end
---
+++ lib/dry_object_mapper/mapper.rb
@@ -0,0 +1,47 @@
+module DryObjectMapper
+  class Mapper
+    @@definitions = {}
+
+    def self.definitions
+      @@definitions
+    end
+
+    def self.call(model_object, struct_dto, options = {})
+      if @@definitions[struct_dto]
+        schema_definition = @@definitions[struct_dto]
+      else
+        schema_definition = DryStructParser::StructSchemaParser.new.call(struct_dto).keys
+        @@definitions[struct_dto] = schema_definition
+      end
+
+      if model_object.respond_to?(:each)
+        model_object = model_object.to_a
+        result = model_object.map {|it| struct_dto.new(get_model_hash_from_definition(it, schema_definition, options)) }
+      else
+        result = struct_dto.new(get_model_hash_from_definition(model_object, schema_definition, options))
+      end
+
+      result
+    end
+
+    def self.get_model_hash_from_definition(model_object, schema_definition, options)
+      result = {}
+      schema_definition.each do |field_name, definition|
+        if options&.dig(field_name).present? && !options.dig(field_name).is_a?(Hash)
+          result[field_name] = options[field_name]
+        elsif definition[:type] == 'hash'
+          result[field_name] = get_model_hash_from_definition(model_object.send(field_name), definition[:keys], options&.dig(field_name))
+        elsif definition[:type] == 'array' && definition[:keys].present?
+          result[field_name] = []
+          model_object.send(field_name).each do |object|
+            result[field_name] << get_model_hash_from_definition(object, definition[:keys], options&.dig(field_name))
+          end
+        else
+          result[field_name] = model_object.send(field_name)
+        end
+      end
+
+      result
+    end
+  end
+end
